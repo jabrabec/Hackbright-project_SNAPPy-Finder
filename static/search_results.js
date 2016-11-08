494c5f7d0aec994146e8122c55884d22b72af1c1
Jennifer Brabec <jenniferbrabec@gmail.com>
2016-11-08 00:30:49 +0000

    search_results.js file cleaned up & comments added

--- static/search_results.js
+++ static/search_results.js
@@ -2,13 +2,15 @@
 
 
 function displayResultsList(result){
-  console.log(result);
   // This clears out the div in case there were previous search results.
   $('#search-results').empty();
   
   // Fill out #search-results div either with results table or 'no results' text
+  // create empty divContents array for holding results
   var divContents = [];
+  // perform the following only if 'results' has content:
   if (result[0]) {
+    // create header row
     var headerRow = '<table>' +
                     '<tr>' +
                     '<th>Name</th>' +
@@ -18,9 +20,11 @@ function displayResultsList(result){
                     '<th>Zipcode</th>' +
                     '<th>Distance (mi)</th>' +
                     '</tr>';
+    // add header row to divContents holding array
     divContents.push(headerRow);
+    // iterate over items in JSON-formatted results
     for (var i in result) {
-      // create a temporary holding string to concatenate html
+      // create a temporary holding string to concatenate html for a single row
       var tempString = '<tr><td>' + result[i][0] + 
                       '</td><td>' + result[i][3] +
                       ' ' + result[i][4] +
@@ -29,35 +33,38 @@ function displayResultsList(result){
                       '</td><td>' + result[i][7] + 
                       '</td><td>' + result[i][8] + 
                       '</tr>';
+      // add this row to the divContents holding array
       divContents.push(tempString);
     }
+    // finalize table tag
     divContents.push('</table>');
+    // concatenate all results in divContents into a single string
     divContents = divContents.join('');
+  // if not result[0], simply state "no results found"
   } else {
     divContents.push('No results found.');
   }
+  // update the contents on main page of div id="search-results"
   $('#search-results').html(divContents);
 }    
 
 
 function submitCoords(position) {
-  // Grab the lat/long/search range values
+  // get the lat/long/search range values via HTML5 geolocation
   var latitude = position.coords.latitude;
-  console.log(latitude);
   var longitude = position.coords.longitude;
-  console.log(longitude);
   var searchRange = $('#coords-search-range').val();
-  console.log(searchRange);
-  // Creating the data to be sent with the AJAX request
+  // set data parameters for sending with the AJAX request
   var params = {'latitude': latitude, 'longitude': longitude, 'searchRange': searchRange };
-  // The AJAX get request, passing in data, referring to success handler.
+  // send AJAX get request, passing in data, referring to success handler.
   $.get('/search-coords.json', params, displayResultsList);
 }
 
-
+// named to avoid confusion with gmaps function
 function alsoGetLocation(evt) {
   evt.preventDefault();
 
+  // get HTML5 geolocation and pass to submitCoords function
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(submitCoords);
   }
@@ -69,21 +76,17 @@ $('#coords-search').on('click', alsoGetLocation);
 
 function submitAddress(evt) {
   evt.preventDefault();
-  
+  // get the address & search range values entered into text fields by user
   var street = $('#street').val();
-  console.log(street);
   var city = $('#city').val();
-  console.log(city);
   var state = $('#state').val();
-  console.log(state);
   var searchRange = $('#addr-search-range').val();
-  console.log(searchRange);
-  // Creating the data to be sent with the AJAX request
+  // set data parameters for sending with the AJAX request
   var params = {'street': street,
                 'city': city,
                 'state': state,
                 'searchRange': searchRange };
-  // The AJAX get request, passing in data, referring to success handler.
+  // send AJAX get request, passing in data, referring to success handler.
   $.get('/search-address.json', params, displayResultsList);
   }
 
