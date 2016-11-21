45afb261b5204d432c7876910ffef3daa2c37504
Jennifer Brabec <jenniferbrabec@gmail.com>
2016-11-21 23:16:34 +0000

    unique IDs implemented for send_mail.js form elements

--- static/send_mail.js
+++ static/send_mail.js
@@ -2,25 +2,25 @@ function getEmail(evt) {
     evt.preventDefault();
 
     var yelpID = $(this).data()['yelpid'];
-    $('#td-' + yelpID).append('<div id="email-form"><form action="#" \
-                                method="POST"><input type="text" id="email" \
+    $('#td-' + yelpID).append('<div id="email-form' + yelpID + '"><form action="#" \
+                                method="POST"><input type="text" id="email' + yelpID + '" \
                                 placeholder="email@domain.com" required><input \
                                 type="submit" value="Send" \
-                                id="submit-email"></form></div>');
-    $('#email').focus();
-    $('#submit-email').click( function(evt){
+                                id="submit-email' + yelpID + '"></form></div>');
+    $('#email' + yelpID).focus();
+    $('#submit-email' + yelpID).click( function(evt){
         evt.preventDefault();
         sendEmail(yelpID);
     });
 }
 
 function sendEmail(yelpID) {
-    var recipient = $('#email').val();
+    var recipient = $('#email' + yelpID).val();
     var params = {'yelpID': yelpID, 'recipient': recipient};
     $.post('/send-mail', params, processResponse);
-    $('#email-form').html("Sending message...");
+    $('#email-form' + yelpID).html("Sending message...");
 }
 
 function processResponse(success_result) {
-    $('#email-form').html(success_result).fadeOut(3000);
+    $('#email-form' + yelpID).html(success_result).fadeOut(3000);
 }
