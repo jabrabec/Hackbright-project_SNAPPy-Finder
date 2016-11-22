0870ef9e62bb743e82b5a43784ac07652c5c0126
Jennifer Brabec <jenniferbrabec@gmail.com>
2016-11-22 07:12:31 +0000

    incorporation of ratings span elements to yelp query search results for proper display of star ratings in progress

--- static/yelp_query.js
+++ static/yelp_query.js
@@ -1,5 +1,10 @@
 function displayYelpReviews(resultString) {
     var rowContents = [];
+    // conversion of rating values like 3.5 to 3-5 in progress
+    // var reviewRating = resultString[1].rating;
+    // console.log("review rating is: " + reviewRating);
+    // var ratingClass = reviewRating.replace('.', '-');
+    // console.log("rating class is: " + ratingClass);
     yelpInformation = '<ul><img src="static/img/yelp-2c-outline.png"' +
                     ' class="yelp-logo">Overall rating: ' +
                     resultString[1].rating +
