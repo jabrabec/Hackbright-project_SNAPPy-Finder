70c5bf2906d252359518149a2ac9388fec1eb535
Jennifer Brabec <jenniferbrabec@gmail.com>
2016-11-28 03:54:08 +0000

    comments added to misc_listeners.js

--- static/misc_listeners.js
+++ static/misc_listeners.js
@@ -1,3 +1,4 @@
+// alternate the display of search-by-coords or search-by-address forms on homepage
 $('#toggle-coords-search').click(function() {
     $('#search-by-coords').css('display', 'none');
     $('#search-by-address').css('display', 'block');
@@ -8,6 +9,10 @@ $('#toggle-addr-search').click(function() {
     $('#search-by-address').css('display', 'none');
 });
 
+// when getting to /search route via 'search by address' button on index page,
+// simulates a button click to toggle search-by-address form on homepage upon
+// completed page load - /search route thus shows appropriate form type depending
+// on how user got there
 $(document).ready(function() {
     if(window.location.hash == "#addr") {
         $('#toggle-coords-search').trigger('click');
