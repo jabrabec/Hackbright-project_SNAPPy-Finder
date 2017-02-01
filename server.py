422b9654604d8a52e44fd6576cbb5c843f91bab7
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-02-01 04:38:53 +0000

    modified debug parameter -- debugging disabled in deployed app

--- server.py
+++ server.py
@@ -160,5 +160,6 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
     PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
 
-    app.run(host="0.0.0.0", port=PORT)
+    app.run(host="0.0.0.0", port=PORT, debug=DEBUG)
